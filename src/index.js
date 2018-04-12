7d97e6f9280e16820377a25f73d0775b75f82aa8
tommy.han <tommy.han@dealertrack.com>
2018-04-12 03:13:23 -0400

    add bottom icons

--- src/index.js
+++ src/index.js
@@ -9,7 +9,14 @@ function Tweet(){
             <div className="content">
                 <NameWithHandle />
                 <Message />
+                <div className="buttons">
+                    <ReplyButton />
+                    <ReTweetButton />
+                    <LikeButton />
+                    <MoreOptionsButton />
+                </div>
             </div>
+
         </div>
     )
 }
@@ -40,5 +47,24 @@ function NameWithHandle(){
     );
 }
 
+const Time = () => (
+    <span className="time"> 3h ago </span>
+);
+
+const ReplyButton = () => (
+    <i className="fa fa-reply reply-button"/>
+);
+
+const ReTweetButton = () => (
+    <i className="fa fa-retweet retweet-button"/>
+);
+
+const LikeButton = () => (
+    <i className="fa fa-heart like-button"/>
+);
+
+const MoreOptionsButton = () => (
+    <i className="fa fa-ellipsis-h more-options-button"/>
+);
 
 ReactDOM.render(<Tweet />, document.querySelector('#root'));
